789489adb507d0d7189f8d6471cb65c2871aef41
Ali2210 <Alideveloper95@gmail.com>
2023-08-02 20:35:31 +0500

    fixed serde error

---
+++ roombot/gpt_text/src/lib.rs
@@ -0,0 +1,70 @@
+
+/// This module provide wrapper plugins to connect with openai 
+
+pub mod openai{
+
+    // import fieri 
+
+    use fieri::{Client, Error};
+    use fieri::completion::{Completion, CompletionParamBuilder, create};
+
+
+    pub struct OpenAICredentials{
+
+        input : String,                          // meaning of my name !
+        end_user : String,                       // abc78821-hkk789
+        max_token : i32,                        // [0-500] 
+    }
+
+    impl OpenAICredentials{
+
+
+        pub fn new(input : String, end_user : String, max_token : i32) -> OpenAICredentials{
+        
+            OpenAICredentials{
+                input,
+                end_user,
+                max_token
+            }
+        }
+
+        /// openai_text_wrapper is a very basic function that solve user query linearly meaning , no additional parameters configuration
+        /// similar like web search 
+        pub async fn openai_text_wrapper(&mut self) -> Result<Completion, Error>{
+
+
+            let client_fieri : _ = Client::new();
+            let parameters : _ = CompletionParamBuilder::new("davinci")
+            .prompt(self.input.clone())
+            .temperature(0.5)
+            .user(self.end_user.clone())
+            .build()?;
+
+            let response : _ = create(&client_fieri, &parameters);
+            response.await
+        }
+
+
+        /// openai_openend is most complex function which solve query as equation. Here model reconfigure itself for finding optimal solution 
+        /// composition of poetry or write blogs   
+        pub async fn openai_openend(&mut self) -> Result<Completion, Error>{
+
+            let client_fieri : _ = Client::new();
+            let parameters : _ = CompletionParamBuilder::new("text-Davinci-003")
+            .prompt(self.input.clone())
+            .top_p(0.92)
+            .max_tokens(self.max_token)
+            .temperature(0.5)
+            .frequency_penalty(0.5)
+            .presence_penalty(0.5)
+            .user(self.end_user.clone())
+            .build()?;
+
+            let response : _ =  create(&client_fieri, &parameters);
+            response.await
+        }
+
+    }
+
+    
+}
--- roombot/src/main.rs
+++ roombot/src/main.rs
@@ -1,21 +1,36 @@
-use actix_web::{get, web, App, HttpServer,HttpRequest, Responder, Result};
+use actix_web::{get, post, web, App, HttpServer, Responder, Result};
 use actix_files::{NamedFile};
+use serde::{Deserialize, Serialize};
+
+
+
+#[derive(Deserialize)]
+struct TranslateFormData {
+
+    query : String,
+    
+}
+
+
 
 #[get("/")]
 async fn index() -> impl Responder {
 
+    
     NamedFile::open_async("./static/index.html").await
 }
 
 #[get("/utopia")]
 async fn image_utopia() -> impl Responder{
 
+    
     NamedFile::open_async("./static/assets/utopia.jpg").await
 }
 
 #[get("/futuristic")]
 async fn image_learning() -> impl Responder{
 
+    
     NamedFile::open_async("./static/assets/translation.png").await
 }
 
@@ -23,24 +38,37 @@ async fn image_learning() -> impl Responder{
 #[get("/translation")]
 async fn translator() -> impl Responder {
 
+    
     NamedFile::open_async("./static/translate.html").await
 }
 
-#[get("/user/register")]
+#[post("/translation/user/{output}")]
+async fn word2word(form : web::Form<TranslateFormData>) -> impl Responder{
 
+    let input : _ =  &form.query;
+
+    //let secret : _ = "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".to_string();
+    
+    format!("Input  = {:?}", input)
+}
+
+#[get("/user/register")]
 async fn register_user() -> impl Responder{
 
+    
     NamedFile::open_async("./static/register.html").await
 }
 
 #[get("/user/history")]
 async fn history() -> impl Responder {
 
+    
     NamedFile::open_async("./static/history.html").await
 }
 
 #[get("/user/invoice")]
 async fn invoice() -> impl Responder {
+
 
     NamedFile::open_async("./static/invoice.html").await
 }
@@ -48,12 +76,14 @@ async fn invoice() -> impl Responder {
 #[get("/user/poetry/topics")]
 async fn add_topic() -> impl Responder{
 
+    
     NamedFile::open_async("./static/poetry.html").await
 }
 
 #[get("/configurations")]
 async fn configurations() -> impl Responder{
 
+    
     NamedFile::open_async("./static/interactive.html").await
 }
 
@@ -66,6 +96,7 @@ async fn configurations() -> impl Responder{
             .service(image_utopia)
             .service(image_learning)
             .service(translator)
+            .service(word2word)
             .service(register_user)
             .service(history)
             .service(invoice)
