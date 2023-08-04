213c1e702e416198fb0013c6ea0b507fb83f627c
Ali2210 <Alideveloper95@gmail.com>
2023-08-04 11:01:29 +0500

    check text have not contains shell code

--- roombot/src/main.rs
+++ roombot/src/main.rs
@@ -2,6 +2,8 @@ use actix_web::{get, post, web, App, HttpServer, Responder, HttpResponse, Result
 use actix_files::{NamedFile};
 use serde::{Deserialize, Serialize};
 use gpt_text ::{openai};
+use regex::Regex;
+
 
 
 #[derive(Deserialize)]
@@ -47,16 +49,59 @@ async fn word2word(form : web::Form<TranslateFormData>) -> HttpResponse{
     let input : _ =  &form.query;
     let apikey : _ = &form.call; 
 
+    let _ = vec!["eval", "echo", "system", "exec", "os", "kill", "script", "wget", "curl", 
+                        "sudo", "cd", "chmod", "rm", "ls","cat", "rmdir", "grep", "tail", 
+                        "mv", "chdir", "chown", "passwd", "unmask", "pwd", "mkdir", "clear", "cp",
+                        "head", "whoami", "copy", "env"];
+
+    let _ = vec!["nude", "porn", "xxx","sexy", "sex", "sexual"];
+
+    let lines = input.lines();
+    let bregex = Regex::new(r"\b(eval | echo | system |exec | os | kill | script | wget | curl | sudo | cd | chmod | rm | ls | cat | rmdir | grep | tail | mv | chdir | chown | passwd | unmask | pwd | mkdir | clear| cp | head | whoami | copy | env )").unwrap();
+    let xregex = Regex::new(r"\b(nude | porn | xxx | sexy | sex | sexual )").unwrap();
+
+
+    let mut take_action : bool = false;
+
+    for words in lines{
+
+        // for bad actors who invade system
+        if bregex.is_match(words){
+            take_action = true;
+            break
+        }
+
+        // for bad boys
+        if xregex.is_match(words){
+            take_action = true;
+            break
+        }
+    }
+
+    if take_action{
+
+        println!("Queries have some bad words which are not acceptable by model");
+        HttpResponse::BadRequest().body(format!("Queries have some bad words which are not acceptable by model"));
+    }
+
+
+    if !input.contains("translation") || !input.contains("translate"){
+
+        println!("Sorry translation prompt will translate message for you ");
+        HttpResponse::BadRequest().body(format!("Sorry translation prompt will translate message for you "));
+    }
+
+
     let mut opencall : _ = openai::new(input.to_string(), "".to_string(), input.len().try_into().unwrap());
     
     let response =  match opencall.openai_text_wrapper(apikey.to_string()).await{
 
-        Ok(resp) => resp,
-        Err(e) => panic!("Error = {:?}", e),
+            Ok(resp) => resp,
+            Err(e) => panic!("Error = {:?}", e),
     };
 
-    HttpResponse::Ok().body(response)
-    
+    HttpResponse::Ok().body(format!("{:?}, {:?}", input.to_string(), response))
+
 }
 
 #[get("/user/register")]
@@ -86,6 +131,11 @@ async fn add_topic() -> impl Responder{
     
     NamedFile::open_async("./static/poetry.html").await
 }
+
+// #[get("/user/poetry/topics/{output}")]
+// async fn poetry() -> HttpResponse{
+
+// }
 
 #[get("/configurations")]
 async fn configurations() -> impl Responder{
