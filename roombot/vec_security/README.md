a77b7b567c4facad55d566853ecbfe3c2e569423
Ali2210 <Alideveloper95@gmail.com>
2023-08-08 17:56:44 +0500

    add security features

---
+++ roombot/vec_security/README.md
@@ -0,0 +1,28 @@
+# vector_security 
+
+    Security is the most crusial part of development. Around 80% brenches is due to negligence, bad practices, old dependenices, poor versioning, credentials leaks and behaviour will manipluation through special kinds of flows that will cause many problems. Null_pointer, dirty_cow, heartbleed, sybill attack, ddos, man of middle attack, deep-fakes are common. 
+
+    warning : ensure security at application, database, web-interface, hosting and module level.
+
+
+# module 
+    
+        vec_sec
+  
+# documentation 
+
+        cargo doc --open 
+        
+        or 
+        
+        visit crates.io/vec_security
+
+# Install
+
+        cargo add vec_security 
+        
+# Maintainance 
+
+        WISDOMENIGMA
+
+
---
+++ roombot/vec_security/src/lib.rs
@@ -0,0 +1,59 @@
+
+/// Vector_security ensure that user data remain secure. 
+/// These module build a unique signature against users.
+pub mod vec_security {
+
+    // import 
+    use crypto_hash::{Algorithm, Hasher};
+    use std::io::Write;
+    use regex::{Regex};
+    
+
+    #[derive(Debug)]
+    /// Authorization is a class where only one field exit, that is data. That data transforme into mathematical notion called hashing  
+    pub struct Authorization{
+
+        data : String,
+
+    }
+
+    /// new_auth is a public method which takes data as argument and return authorize object.
+    pub async fn new_auth(data : String) -> Authorization{
+        Authorization{
+            data,
+        }
+    }
+
+    impl Authorization{
+
+
+        /// Create new hash which is act as receiver and return vector.  
+        pub async fn create_new_hash(&mut self) -> Vec::<u8>{
+
+            let mut hash_data : _ = Hasher::new(Algorithm::SHA256);
+            let _ = hash_data.write_all((self.data).as_bytes());
+
+            hash_data.finish()
+        }
+
+
+        /// verified is a special function that return signature is authorize or not 
+        pub async fn verified(&mut self, data : Vec::<u8>) -> bool{
+
+            
+
+            let word : _ = String::from_utf8(data).unwrap();
+            let pattern : _ = Regex::new("#[a-z]([0-9])").unwrap();
+            
+           if pattern.is_match(&word) {
+
+                return true;
+           }
+
+           false
+
+        }
+    }
+}
+
+
